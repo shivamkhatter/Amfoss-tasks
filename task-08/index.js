c6e130645c1fb1623ba7fa25fa775e96d767af44
shivamkhatter <shivamkhatter04@gmail.com>
2022-12-14 19:12:31 +0530

    ..

---
+++ task-08/index.js
@@ -0,0 +1,14 @@
+var crash = new Audio()
+crash.src = "/home/shivam/Desktop/Amfoss-tasks/task-08/sounds/crash.mp3"
+var kickbass = new Audio()
+kickbass.src = "/home/shivam/Desktop/Amfoss-tasks/task-08/sounds/kickbass.mp3"
+var snare = new Audio()
+snare.src = "/home/shivam/Desktop/Amfoss-tasks/task-08/sounds/snare.mp3"
+var tom1 = new Audio()
+tom1.src = "/home/shivam/Desktop/Amfoss-tasks/task-08/sounds/tom1.mp3"
+var tom2 = new Audio()
+tom2.src = "/home/shivam/Desktop/Amfoss-tasks/task-08/sounds/tom2.mp3"
+var tom3 = new Audio()
+tom3.src = "/home/shivam/Desktop/Amfoss-tasks/task-08/sounds/tom3.mp3"
+var tom4 = new Audio()
+tom4.src = "/home/shivam/Desktop/Amfoss-tasks/task-08/sounds/tom4.mp3"
